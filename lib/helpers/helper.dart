a2cbee0938b7de116a09b308f2ef4a3d5e2ef652
OMi Shah <omi@codecyan.com>
2025-07-03 10:29:17 +0530

    added comments

--- lib/helpers/helper.dart
+++ lib/helpers/helper.dart
@@ -8,12 +8,14 @@ import 'package:http/http.dart' as http;
 import 'package:url_launcher/url_launcher_string.dart';
 
 abstract class Helper {
+  // open link in external browser
   static Future<void> openLink(String url) async {
     if (!await launchUrlString(url, mode: LaunchMode.externalApplication)) {
       throw Exception('Could not launch $url');
     }
   }
 
+  // make http GET request call
   static Future<ApiResponse> httpGetRequest(String url) async {
     final headers = {HttpHeaders.acceptHeader: 'application/json'};
 
@@ -34,6 +36,7 @@ abstract class Helper {
     }
   }
 
+  // show snackbar at ease
   static void showSnackbar(BuildContext context, {String? text}) {
     final snackBar = SnackBar(content: Text(text ?? 'Something went wrong!'));
 
